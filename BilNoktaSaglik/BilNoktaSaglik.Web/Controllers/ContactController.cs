af7f664d5c114b185694de681e777ba0076c9a02
YAREN SU YAVUZ <yarensuyavuz@posta.mu.edu.tr>
2024-03-04 20:42:55 +0300

    Added Contact

---
+++ BilNoktaSaglik/BilNoktaSaglik.Web/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BilNoktaSaglik.Web.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult ContactIndex()
+        {
+            return View();
+        }
+    }
+}
