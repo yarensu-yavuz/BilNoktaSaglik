82919b2180e460d1400ba4df8a195ab0c8bfc8e4
YAREN SU YAVUZ <yarensuyavuz@posta.mu.edu.tr>
2024-03-04 20:37:40 +0300

    Proje bu alana ilk defa taşındı

---
+++ BilNoktaSaglik/BilNoktaSaglik.Web/Controllers/HomePageController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BilNoktaSaglik.Web.Controllers
+{
+    public class HomePageController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+     
+        public IActionResult PageLayoutDesign()
+
+
+        {
+            return View();
+        }
+    }
+}
---
+++ BilNoktaSaglik/BilNoktaSaglik.Web/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BilNoktaSaglik.Web.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
